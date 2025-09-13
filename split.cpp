8c15fd1487b0c44cc3fe2eb4770c25e8095bb88e
Janessa Techathamawong <techatha@usc.edu>
2025-09-13 05:42:42 +0000

    Initial commit for hw1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,39 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
+void appendRecursive(Node*& list, Node* node);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  // WRITE YOUR CODE HERE
+  if (in == NULL) {
+    return;
+  }
+
+  // Save the current node and move input pointer forward
+  Node* curr = in;
+  in = in->next;
+  curr->next = NULL;
+
+  // Place the current node into the correct list (odd/even)
+  if (curr->value % 2 == 0) {
+    appendRecursive(evens, curr)
+  } else {
+    appendRecursive(odds, curr)
+  }
+
+  // Recursive call on the remainder of the list
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+void appendRecursive(Node*& list, Node* node) {
+  if (list == NULL) {
+    list = node;
+  } else {
+    appendRecursive(list->next, node)
+  }
+}
